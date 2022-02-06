0be6199b3d9ca222fd9bc903e0a1fdc0861bee1f
Chinanago <48302933+Chinanago@users.noreply.github.com>
2022-02-06 20:08:14 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,179 @@
 window.onload = function () {
-        var URL = window.URL || window.webkitURL;
         var isError = true;
-        var selectedFrame = 0;
-        var timeStamp = new Array();
-            /**
-             * 動画選択時に再生する
-             */
-            playSelectedFile = function playSelectedFileInit(event) {
-                var file = event.target.files[0];
-                var videoNode = document.querySelector('video');
-                isError = (videoNode.canPlayType(file.type) === '' ? true : false);
-                if (isError) {
-                    return;
-                }
-                videoNode.src = URL.createObjectURL(file); // inputで選択した動画を再生する
-            };
-            inputNode = document.querySelector('input');
+        var selected = -1; //-1 means unselected
+        var timeStamp = new Array(); //time stamp is controled by this array. id of canvas tag is Int.
 
-        if (!URL) {
-            alert("Error! Your browser is not supported.");
-            return;
-        }
-        inputNode.addEventListener('change', playSelectedFile, false);
+        function popupInit(){
+            var popup = document.getElementById('js-popup');
+            if(!popup) return;
+            popup.classList.add('is-show');
+          
+            var blackBg = document.getElementById('js-black-bg');
 
-        function capture() {
-            if(!isError){
-                var cEle = document.createElement('canvas');
-                var cCtx = cEle.getContext('2d');
-                var vEle = document.querySelector('video');
-                
-                cEle.id = String(vEle.currentTime);
-                cEle.style = "width:60px; height: 40px;"; //実際の表示サイズ
-                cEle.width = vEle.videoWidth; // canvasの幅と高さを、動画の幅と高さに合わせる
-                cEle.height = vEle.videoHeight;
-    
-                timeStamp.push(cEle.id);
-                
-                cEle.addEventListener('click', {time: parseFloat(cEle.id), handleEvent: select}, false);
-                cEle.addEventListener('click', {id: cEle.id, handleEvent: setBorder}, false);
-                cCtx.drawImage(vEle, 0, 0); // canvasに関数実行時の動画のフレームを描画
-    
-                var galary = document.getElementById('galary');// galaryエリアに新しく画像を追加
-                if(selectedFrame != 0){
-                    currentFrame = document.getElementById(selectedFrame);
-                    galary.insertBefore(cEle, currentFrame);
-                    bye();
-                }else{
-                    galary.appendChild(cEle);
+            closePopUp(blackBg);
+
+            function closePopUp(elem) {
+              if(!elem) return;
+              elem.addEventListener('click', function() {
+                if(isError != true){
+                    popup.classList.remove('is-show');
                 }
+              })
             }
         }
 
-        function select(){
-            var vEle = document.querySelector('video');
-            vEle.currentTime = this.time;
-            selectedFrame = this.time;
+        function videoInputAndplayInit(){
+            var URL = window.URL || window.webkitURL;
+            var filearea = document.getElementById('dropArea');
+            var fileInput = document.getElementById('file_upload');
+
+            filearea.addEventListener('dragover', function(e){
+                e.preventDefault();
+                filearea.classList.add('dragover');
+            });
+            filearea.addEventListener('dragleave', function(e){
+                e.preventDefault();
+                filearea.classList.remove('dragover');
+            });
+            filearea.addEventListener('drop', function(e){
+                e.preventDefault();
+                filearea.classList.remove('dragover');
+
+                var files = e.dataTransfer.files;
+                fileInput.files = files;
+            });
+            inputNode = document.querySelector('input');
+
+            inputNode.addEventListener('change', function(e){
+                var file = e.target.files[0];
+                var videoNode = document.querySelector('video');
+                var popup = document.getElementById('js-popup');
+                //再生可能か判定
+                isError = (videoNode.canPlayType(file.type) === '' ? true : false);
+                if(isError) return;
+                videoNode.src = URL.createObjectURL(file); // inputで選択した動画
+                videoNode.pause();
+                popup.classList.remove('is-show');
+            }, false);
+            if(!URL){
+                alert("Error! Your browser is not supported.");
+                return;
+            }
         }
-        function setBorder(){
-            var selectedImg = document.getElementById(this.id);
-            if(selectedImg.style.borderWidth == "3px"){
-                selectedImg.style.borderWidth = "0px";
-            }else{
-                selectedImg.style.border = "blue solid 3px";
-            };
+
+        popupInit();
+        videoInputAndplayInit();
+
+        function capture() {
+            console.log(timeStamp);
+            if(!isError){
+                /* 
+                次のようなhtmlを追加する
+                    <div class="thumblock" id="連番の整数値">
+                        <canvas class="thumbnail" style="width: 60px; height: 40px;">
+                        <p>"id+1の整数番号"</p>
+                    </div>
+                */
+                var blockElement = document.createElement('div');
+                var flagElement = document.createDocumentFragment();
+
+                var gallery = document.querySelector('.gallery'); // galleryエリアに新しく画像を追加
+                var picElement = document.createElement('canvas');
+                var picContext = picElement.getContext('2d');
+                var videoElement = document.querySelector('video');
+                var time = videoElement.currentTime;
+
+                var captionElement = document.createElement('p');
+
+                captionElement.classList.add('caption');
+                blockElement.classList.add('thumblock');
+
+                picElement.style = "width:60px; height: 40px;"; //実際の表示サイズ
+                picElement.width = videoElement.videoWidth; // canvasの幅と高さを、動画の幅と高さに合わせる
+                picElement.height = videoElement.videoHeight;
+                picElement.classList.add('thumbnail');
+
+                picElement.addEventListener('click', (function(){
+                    for(let i=0;i<timeStamp.length;i++){
+                        var tempCanvas = document.getElementById(String(i)).firstChild;
+                        tempCanvas.classList.remove('selected');
+                    };
+                    if(selected == parseInt(blockElement.id)){
+                        selected = -1;
+                    }else{
+                        selected = parseInt(blockElement.id);
+                        videoElement.currentTime = timeStamp[selected];
+                        picElement.classList.add('selected');
+                    };
+                }), false);
+
+                picContext.drawImage(videoElement,0,0);     
+                if(selected == -1){
+                    picElement.classList.remove('selected');
+                    blockElement.id = String(timeStamp.length); //Arrayの先頭のindex
+                    captionElement.append(document.createTextNode(String(timeStamp.length+1)));
+                    timeStamp.push(time);
+
+                    flagElement.append(picElement);
+                    flagElement.append(captionElement);
+                    blockElement.appendChild(flagElement);
+                    gallery.appendChild(blockElement);
+                }else{
+                    blockElement.id = String(selected); //選ばれている要素の肩代わり
+                    picElement.classList.add('selected');
+                    var selectedBlock = document.getElementById(String(selected));
+                    captionElement.append(document.createTextNode(String(selected+1)));                    
+                    picContext.drawImage(videoElement, 0, 0); // canvasに関数実行時の動画のフレームを描画
+
+                    flagElement.append(picElement);
+                    flagElement.append(captionElement);
+                    blockElement.appendChild(flagElement);
+                    gallery.insertBefore(blockElement,selectedBlock);
+                    selectedBlock.remove();
+                    timeStamp[selected] = time;
+                }
+            }
         }
         function imgDownload(){
-            var canvas = document.getElementById(timeStamp.slice(-1)[0]);
-            var a = document.createElement('a');
-            a.href = canvas.toDataURL("image/jpg");
-            a.download = timeStamp.slice(-1)[0]+".jpg";
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
+            if(selected != -1){
+                var block = document.getElementById(String(selected));
+                var a = document.createElement('a');
+                a.href = block.firstChild.toDataURL("image/jpg");
+                a.download = timeStamp.slice(-1)[0]+".jpg";
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+            }else{
+                alert('画像を選んでください');
+            }
         }
-        function bye(){
-            selectedCanvas = document.getElementById(String(selectedFrame));
-            selectedCanvas.remove();
-            selectedFrame = 0;
+        function imgRemove(){
+            if(selected != -1){
+                var targetBlock = document.getElementById(String(selected));
+                targetBlock.remove();
+                timeStamp.splice(selected,1);
+                allBlock = document.querySelectorAll('.thumblock');
+                allCaption = document.querySelectorAll('.caption');
+                for(let i=0;i<timeStamp.length;i++){
+                    allBlock[i].id = String(i);
+                    allCaption[i].innerText = String(i+1);
+                }
+                var selectedBlock = document.getElementById(String(selected));
+                var videoElement = document.querySelectorAll('video');
+                videoElement.currentTime = timeStamp[selected];
+                selectedBlock.firstChild.classList.add('selected');
+            // selected = -1;
+            }
+        }
+        function imgProcess(){
+            if(selected!=-1){
+                canvas = du
+            }
         }
         var captureButton = document.getElementById('capture');
         var downloadButton = document.getElementById('download');
         var removeButton = document.getElementById('remove');
         captureButton.addEventListener('click', capture, false);
         downloadButton.addEventListener('click', imgDownload, false);
-        removeButton.addEventListener('click', bye, false);
+        removeButton.addEventListener('click', imgRemove, false);
 };
